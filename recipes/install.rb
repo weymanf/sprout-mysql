7501fffff8a5c6e9237b295ab586595eeb2bb06e
Abhijit Hiremagalur <pair+abhijit@pivotal.io>
2014-12-20 11:08:49 -0800

    Replace node['current_user'] with node['sprout']['user']
    In Chef 12 node['current_user'] is root under sudo, we want this to be the normal user (e.g. pivotal) so we're using ENV['SUDO_USER'] instead. Several recipes that manage file permissions rely on this.
    
    See https://github.com/pivotal-sprout/sprout-base/issues/3 for more info

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,17 +2,17 @@
 require 'pathname'
 
 PASSWORD = node['sprout']['mysql']['root_password']
-# The next two directories will be owned by node['current_user']
+# The next two directories will be owned by node['sprout']['user']
 DATA_DIR = '/usr/local/var/mysql'
 PARENT_DATA_DIR = '/usr/local/var'
 
 include_recipe 'sprout-base::homebrew'
 
-["/Users/#{node['current_user']}/Library/LaunchAgents",
+["/Users/#{node['sprout']['user']}/Library/LaunchAgents",
  PARENT_DATA_DIR,
  DATA_DIR].each do |dir|
   directory dir do
-    owner node['current_user']
+    owner node['sprout']['user']
     action :create
   end
 end
@@ -24,7 +24,7 @@ ruby_block 'copy mysql plist to ~/Library/LaunchAgents' do
     active_mysql = Pathname.new('/usr/local/bin/mysql').realpath
     plist_location = (active_mysql + '../../' + 'homebrew.mxcl.mysql.plist').to_s
     destination = "#{node['sprout']['home']}/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
-    system("cp #{plist_location} #{destination} && chown #{node['current_user']} #{destination}") || fail("Couldn't find the plist")
+    system("cp #{plist_location} #{destination} && chown #{node['sprout']['user']} #{destination}") || fail("Couldn't find the plist")
   end
 end
 
@@ -33,14 +33,14 @@ ruby_block 'mysql_install_db' do
     active_mysql = Pathname.new('/usr/local/bin/mysql').realpath
     basedir = (active_mysql + '../../').to_s
     data_dir = '/usr/local/var/mysql'
-    system("mysql_install_db --verbose --user=#{node['current_user']} --basedir=#{basedir} --datadir=#{DATA_DIR} --tmpdir=/tmp && chown #{node['current_user']} #{data_dir}") || fail('Failed initializing mysqldb')
+    system("mysql_install_db --verbose --user=#{node['sprout']['user']} --basedir=#{basedir} --datadir=#{DATA_DIR} --tmpdir=/tmp && chown #{node['sprout']['user']} #{data_dir}") || fail('Failed initializing mysqldb')
   end
   not_if { File.exist?('/usr/local/var/mysql/mysql/user.MYD') }
 end
 
 execute 'load the mysql plist into the mac daemon startup thing' do
   command "launchctl load -w #{node['sprout']['home']}/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
-  user node['current_user']
+  user node['sprout']['user']
   not_if { system('launchctl list com.mysql.mysqld') }
 end
 
