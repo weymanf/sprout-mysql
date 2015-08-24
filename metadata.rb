f4bf5e7cacca7309eac2e01146114a3f6d492e27
Abhi Hiremagalur <abhijit@hiremaga.com>
2015-08-24 11:27:23 -0700

    Switch to sprout-maintainers@googlegroups.com

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'sprout-mysql'
 maintainer 'Pivotal'
-maintainer_email 'sprout-maintainers@pivotallabs.com'
+maintainer_email 'sprout-maintainers@googlegroups.com'
 license 'MIT'
 description 'Cookbook for managing mysql on an OSX workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
